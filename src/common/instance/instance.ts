1bb8975079c07cbe79faf473cca725bc2b55acf3
VanoKi <kirsvano@gmail.com>
2025-06-11 18:23:07 +0300

    fix: all files

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from "axios"
 
-const token = '448e0d12-136a-4f40-9ff3-99347c703a57'
-const apiKey = '14d62fb0-7776-4500-94b8-5e06e7d229ae'
+const token = "448e0d12-136a-4f40-9ff3-99347c703a57"
+const apiKey = "14d62fb0-7776-4500-94b8-5e06e7d229ae"
 
 export const instance = axios.create({
-  baseURL: 'https://social-network.samuraijs.com/api/1.1',
+  baseURL: "https://social-network.samuraijs.com/api/1.1",
   headers: {
     Authorization: `Bearer ${token}`,
-    "API-KEY": apiKey
-  }
+    "API-KEY": apiKey,
+  },
 })
--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,18 +1,18 @@
-import {instance} from "@/common/instance/instance.ts";
-import {Todolist} from "@/features/todolists/api/todolistsApi.types.ts";
-import {BaseResponce} from "@/common/types";
+import { instance } from "@/common/instance/instance.ts"
+import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
+import { BaseResponce } from "@/common/types"
 
 export const todolistsApi = {
   getTodolists() {
-    return instance.get<Todolist[]>('/todo-lists')
+    return instance.get<Todolist[]>("/todo-lists")
   },
-  changeTodolistTitle( {id, title}: {id:string, title: string}) {
-    return instance.put<BaseResponce>(`/todo-lists/${id}`, {title})
+  changeTodolistTitle({ id, title }: { id: string; title: string }) {
+    return instance.put<BaseResponce>(`/todo-lists/${id}`, { title })
   },
-  createTodolist(title:string) {
-    return instance.post<BaseResponce<{item: Todolist}>>('/todo-lists',{title})
+  createTodolist(title: string) {
+    return instance.post<BaseResponce<{ item: Todolist }>>("/todo-lists", { title })
   },
-  deleteTodolist(id:string) {
+  deleteTodolist(id: string) {
     return instance.delete<BaseResponce>(`/todo-lists/${id}`)
-  }
+  },
 }
