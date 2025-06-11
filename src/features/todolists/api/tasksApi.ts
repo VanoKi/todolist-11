fd47db75188c52c499ca2d1e509135a8897eaba5
VanoKi <kirsvano@gmail.com>
2025-06-11 19:00:59 +0300

    feat: new files tasks

---
+++ src/features/todolists/api/tasksApi.ts
@@ -0,0 +1,5 @@
+import { instance } from "@/common/instance/instance.ts"
+import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
+import { BaseResponce } from "@/common/types"
+
+export const tasksApi = {}
