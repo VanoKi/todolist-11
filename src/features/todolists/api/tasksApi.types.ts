9173ae6a554a4ebc7ffb74b15b80285d3ab0a09e
VanoKi <kirsvano@gmail.com>
2025-06-11 21:21:05 +0300

    feat: enum

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -23,3 +23,17 @@ export type UpdateTaskModel = {
   startDate: string
   deadline: string
 }
+export enum TaskStatus {
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Draft = 3,
+}
+
+export enum TaskPriority {
+  Low = 0,
+  Middle = 1,
+  Hi = 2,
+  Urgently = 3,
+  Later = 4,
+}
