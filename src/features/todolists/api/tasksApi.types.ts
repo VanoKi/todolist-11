a51e9f2811ccd985198cfb07b92585fc0e825679
VanoKi <kirsvano@gmail.com>
2025-06-11 19:28:16 +0300

    fix: types

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -1,6 +1,17 @@
-export type Todolist = {
-  id: string
+export type DomainTask = {
+  description: string
   title: string
-  addedDate: string
+  status: number
+  priority: number
+  startDate: string
+  deadline: string
+  id: string
+  todoListId: string
   order: number
+  addedDate: string
+}
+export type GetTasksResponse = {
+  error: string | null
+  totalCount: number
+  items: DomainTask[]
 }
