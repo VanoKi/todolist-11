eb86f4d9ed7b4b733362a45215d93371ed3908b4
VanoKi <kirsvano@gmail.com>
2025-06-11 18:55:49 +0300

    feat: add env var to instance

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,9 @@
 import axios from "axios"
 
-const token = "448e0d12-136a-4f40-9ff3-99347c703a57"
-const apiKey = "14d62fb0-7776-4500-94b8-5e06e7d229ae"
-
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
-    "API-KEY": apiKey,
+    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    "API-KEY": import.meta.env.VITE_API_KEY,
   },
 })
